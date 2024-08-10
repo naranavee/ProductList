12eab9cf678da0e9a9d212e2ef5b8473a5bfc27e
naveen <naranaveen576@gmail.com>
2024-08-10 09:02:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/apicallingrecyclerview/ApiInterface.kt
@@ -0,0 +1,14 @@
+package com.example.apicallingrecyclerview
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiInterface {
+
+    @GET("/posts")
+    fun getData():Call<DataModel>
+
+
+
+
+}
